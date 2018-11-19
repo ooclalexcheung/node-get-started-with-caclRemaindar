6f1ba1421a881d2a9763dfe45b02ecd595253190
ooclalexcheung <41135345+ooclalexcheung@users.noreply.github.com>
2018-11-19 16:49:36 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,9 +1,27 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar( x, y) {
+    return x% y;
+};
 
-function caclSum() {};
+function caclSum(inputarray) {
+    var sum =0;
+    for (let i=0; i<inputarray.length; i++)
+    {
+        sum += (inputarray[i]);
+    }
+    return sum;
 
-function caclSumInConditon() {};
+};
+
+function caclSumInConditon(inputarray,number) {
+    var sum =0;
+    for (let i=0; i<inputarray.length; i++)
+    {
+        if(inputarray[i]< number)
+        sum += (inputarray[i]);
+    }
+    return sum;
+};
 
 module.exports = {
     caclRemaindar,
